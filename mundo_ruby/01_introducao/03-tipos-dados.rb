56340b2472725b5338f47b43adfe4d230118871e
J-Breno <joao.breno85@hotmail.com>
2024-11-29 12:59:42 -0300

    Entendendo o tipo de dados no ruby

---
+++ mundo_ruby/01_introducao/03-tipos-dados.rb
@@ -0,0 +1,17 @@
+"Ola".class
+1.class
+1.3.class
+(1 + 3.4)
+true.class
+false.class
+Time.now
+:name
+range = 1..10
+range.each do |item|
+  puts item
+end
+
+range.include? 5
+
+# diferenca de fazer um range assim: 1..10 para assim 1...10 é que com três pontos ele não coloca o 10
+{nome: "Breno", "age" => 32}
