838021c24e2b9e62a88f55c0c1b523b2b2f6c47d
J-Breno <joao.breno85@hotmail.com>
2024-11-29 14:12:58 -0300

    Melhorando o código do desafio

--- mundo_ruby/01_introducao/desafio.rb
+++ mundo_ruby/01_introducao/desafio.rb
@@ -1,51 +1,18 @@
 pessoas = [
-  { pessoa1: {
-    nome: "João",
-    email: "joao@gmail.com"
-  } },
-  { pessoa2: {
-    nome: "Breno",
-    email: "breno@hotmail.com",
-    funcionaria: "Back-end"
-  } },
-{ pessoa3: {
-  nome: "Souza",
-  email: "souza@outlook.com"
-} },
-{ pessoa4: {
-  nome: "Melo",
-  email: "melo@gmail.com",
-  funcionaria: "Front-end"
-} },
-{ pessoa5: {
-  nome: "João Breno",
-  email: "joaobreno@gmail.com"
-} },
-{ pessoa6: {
-  nome: "Breno Souza",
-  email: "brenosoua@hotmail.com",
-  funcionaria: "Full-stack"
-} },
-{ pessoa7: {
-  nome: "Souza Melo",
-  email: "souzamelo@outlook.com"
-} },
-{ pessoa8: {
-  nome: "João Melo",
-  email: "joaomelo@gmail.com"
-} },
-{ pessoa9: {
-  nome: "João Souza",
-  email: "joaosouza@hotmail.com"
-} },
-{ pessoa10: {
-  nome: "Breno Melo",
-  email: "brenomelo@outlook.com"
- }}
+  { pessoa1: { nome: 'joão', email: 'joao@gmail.com' } },
+  { pessoa2: { nome: 'Breno', email: 'breno@hotmail.com', funcionaria: 'Back-end' } },
+  { pessoa3: { nome: 'Souza', email: 'souza@outlook.com' } },
+  { pessoa4: { nome: 'Melo', email: 'melo@gmail.com', funcionaria: 'Front-end' } },
+  { pessoa5: { nome: 'João Breno', email: 'joaobreno@gmail.com' } },
+  { pessoa6: { nome: 'Breno Souza', email: 'brenosoua@hotmail.com', funcionaria: 'Full-stack' } },
+  { pessoa7: { nome: 'Souza Melo', email: 'souzamelo@outlook.com' } },
+  { pessoa8: { nome: 'João Melo', email: 'joaomelo@gmail.com' } },
+  { pessoa9: { nome: 'João Souza', email: 'joaosouza@hotmail.com' } },
+  { pessoa10: { nome: 'Breno Melo', email: 'brenomelo@outlook.com' }}
 ]
 
 def full_informations(pessoa)
-  pessoa.each do |_key, dados|
+  pessoa.each_value do | dados|
     if dados[:funcionaria]
       puts "O #{dados[:nome]} tem o email: #{dados[:email]} e ela é uma funcionário, cujo o seu cargo é #{dados[:funcionaria]}"
     else
@@ -57,4 +24,3 @@ end
 pessoas.each do |pessoa|
   full_informations(pessoa)
 end
-
