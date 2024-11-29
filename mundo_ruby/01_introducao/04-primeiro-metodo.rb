2ad55b0834d887159ff8067f9932c242d4fc6730
J-Breno <joao.breno85@hotmail.com>
2024-11-29 13:24:36 -0300

    Blocks em ruby

--- mundo_ruby/01_introducao/04-primeiro-metodo.rb
+++ mundo_ruby/01_introducao/04-primeiro-metodo.rb
@@ -11,13 +11,28 @@ class Agendamento
     agendamento = self.new(data_hora, descricao)
     agendamento.imprimir_comprovante
   end
-  def imprimir_comprovante
+  def imprimir_comprovante(&block)
     self.impressao = true
+    block.call(self)
     "Impressao"
   end
 end
 
-agendamento = Agendamento.new Time.now, "Descrição"
-puts agendamento.imprimir_comprovante
+# agendamento = Agendamento.new Time.now, "Descrição"
+# puts agendamento.imprimir_comprovante
 
-puts Agendamento.criar_imprimir(Time.now, "Teste")
+# puts Agendamento.criar_imprimir(Time.now, "Teste")
+
+# exibe_descricao_com_nome = lambda do |agendamento|
+#   "Agendamento com Data #{agendamento.data_hora} e Descrição #{agendamento.descricao}"
+# end
+
+agendamento = Agendamento.new(Time.now, "Teste")
+teste = agendamento.imprimir_comprovante() do |agendamento|
+  puts "Agendamento com Data #{agendamento.data_hora} e Descrição #{agendamento.descricao}"
+end
+
+puts teste
+
+# O block seria tipo uma função anônima no js ou um callback, porém diferente do js, o block
+# não precisa ser necessáriamente uma função, mas ele é um bloco de código
